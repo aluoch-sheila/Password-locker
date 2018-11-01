65bf380aa8f0ba97e15af3f493208b5a6bca9309
aluoch-sheila <aluochsheila1999@gmail.com>
2018-11-01 16:04:20 +0300

    imported the unittest module

---
+++ password.py
@@ -0,0 +1,12 @@
+class Password:
+    """
+    Class that generates new instances of Password.
+    """
+
+    def __init__(self,first_name,last_name,password,email):
+
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.email = email
---
+++ password_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from password import Password # Importing the contact class
+
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the password class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
